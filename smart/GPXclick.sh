76958a0806f42d8d1db2ab8cbd7871064cc293c1
marry-xxx <maryamkusumajati@gmail.com>
2024-08-13 17:21:49 +0700

    Create GPXclick.sh

---
+++ smart/GPXclick.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+printf '\033]2;helpbot\a'
+while :
+do
+rm -rf /home/chrome60/Default/Cache/*
+find /root/Downloads -type f -name '.*' -exec rm {} +
+rm -rf  /home/chrome60/BrowserMetrics
+termdown 2
+ google-chrome --user-data-dir=/home/chrome60 https://cerdaspro.com/ --start-maximized --no-sandbox --incognito & sleep 5
+ google-chrome --user-data-dir=/home/chrome60 https://rudgames.com/ --start-maximized --no-sandbox --incognito & sleep 1
+clear
+termdown 17
+clear
+echo start helpbot
+actiona -s -C -Q -e -x /root/Desktop/gpxclick.ascr
+termdown 15
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+apt-get autoclean
+clear
+termdown 3
+/usr/games/sl -l
+termdown 5
+done
