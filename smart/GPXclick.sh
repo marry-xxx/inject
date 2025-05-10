eca99d7ecde2b2f835ec03270d5ded349014efa0
marry-xxx <maryamkusumajati@gmail.com>
2025-05-10 13:33:14 +0700

    Update GPXclick.sh

--- smart/GPXclick.sh
+++ smart/GPXclick.sh
@@ -6,14 +6,14 @@ rm -rf /home/chrome60/Default/Cache/*
 find /root/Downloads -type f -name '.*' -exec rm {} +
 rm -rf  /home/chrome60/BrowserMetrics
 termdown 2
- google-chrome --user-data-dir=/home/chrome60 https://leonigame.online/autoplay --start-maximized --no-sandbox --incognito & sleep 5
+ google-chrome --user-data-dir=/home/chrome60 https://syahgame.com/ --start-maximized --no-sandbox --incognito & sleep 5
  google-chrome --user-data-dir=/home/chrome60 https://rudgame.com/ --start-maximized --no-sandbox --incognito & sleep 1
 clear
 termdown 17
 clear
 echo start helpbot
 actiona -s -C -Q -e -x /root/Desktop/gpxclick.ascr
-termdown 15
+termdown 5
 pkill --oldest chrome
 pkill --oldest chrome
 pkill --oldest chrome
