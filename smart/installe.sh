b55e1f300a95f39389c9f9ff3d20c9840df8ac07
marry-xxx <maryamkusumajati@gmail.com>
2025-05-10 15:00:12 +0700

    installe.sh

--- smart/installe.sh
+++ smart/installe.sh
@@ -97,12 +97,13 @@ wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/link.sh
 #wget https://github.com/marry-xxx/inject/raw/main/smart/GPXNew.sh
 #wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/linkFIGPX.sh
 wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/play.png
-#wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/playgpx2.png
+wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/playgpx2.png
 #chmod +x FIGPX.ascr
 #chmod +x GPXNew.sh
 #chmod +x linkFIGPX.sh
 chmod +x chrc.sh
 chmod +x link.sh
+chmod +x playgpx2.png
 wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/gpx.ascr
 wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/GPXif.sh
 wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/gpxclick.ascr
