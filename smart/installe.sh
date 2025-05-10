a87700bd3027ee0cd137a50edc05a7afc148cfa5
marry-xxx <maryamkusumajati@gmail.com>
2025-05-10 22:40:33 +0700

    Update installe.sh

--- smart/installe.sh
+++ smart/installe.sh
@@ -97,18 +97,18 @@ wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/link.sh
 #wget https://github.com/marry-xxx/inject/raw/main/smart/GPXNew.sh
 #wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/linkFIGPX.sh
 wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/play.png
-wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/playgpx2.png
+wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/playgpx22.png
 #chmod +x FIGPX.ascr
 #chmod +x GPXNew.sh
 #chmod +x linkFIGPX.sh
 chmod +x chrc.sh
 chmod +x link.sh
-chmod +x playgpx2.png
+chmod +x playgpx22.png
 wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/gpx.ascr
 wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/GPXif.sh
 wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/gpxclick.ascr
 wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/GPXclick.sh
-https://raw.githubusercontent.com/marry-xxx/inject/main/smart/gpnew.ascr
+wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/gpnew.ascr
 #wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/gpxads.ascr
 #wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/GPXifads.sh
 #wget https://raw.githubusercontent.com/marry-xxx/inject/main/smart/GPXads2.ascr
